7759a038263c66c7ab523b3c0608905e8ef1d240
Eissa Saber <eissapk44@gmail.com>
2021-05-14 06:26:10 +0200

    updated example - markdown parser

--- src/services/parse.js
+++ src/services/parse.js
@@ -1,10 +1,18 @@
+const lt_regex = /</g;
+const gt_regex = />/g;
+const br_regex = /\n|\r|↵|\u21b5/g;
+
 export function parse(text) {
+  // tags
+  text = text.replace(/<[^>]*>/gi, str => str.replace(lt_regex, "&lt;").replace(gt_regex, "&gt;"));
+
+  //! start MD
   // line
   text = text.replace(/---|___/g, `<hr>`);
   // font styles
-  text = text.replace(/[\*\_]{2}(.*)[\*\_]{2}/gi, "<b>$1</b>");
-  text = text.replace(/[\*\_]{1}(.*)[\*\_]{1}/gi, "<i>$1</i>");
-  text = text.replace(/[\~]{2}(.*)[\~]{2}/gi, "<del>$1</del>");
+  text = text.replace(/([\*\_]{2})(.*)([\*\_]{2})/g, "<b>$2</b>");
+  text = text.replace(/([\~]{2})(.*)([\~]{2})/gi, "<del>$2</del>");
+  text = text.replace(/([\*\_]{1})(.*)([\*\_]{1})/gi, "<i>$2</i>");
   // image - must before link
   text = text.replace(/\!\[([^\]]+)\]\(([^\)]+)\)/gi, `<img src="$2" alt="$1" />`);
   // link
@@ -17,7 +25,21 @@ export function parse(text) {
   text = text.replace(/[#]{2}(.*)/gi, "<h2>$1</h2>");
   text = text.replace(/[#]{1}(.*)/gi, "<h1>$1</h1>");
   // code
-  text = text.replace(/(?<=\n)[\`]{3}[a-z]+/gi, "<pre>");
-  text = text.replace(/[\`]{3}[\n]/g, "</pre>");
+  text = text.replace(/([\`]{3}[a-z]+)/gi, "<pre>");
+  text = text.replace(/([\`]{3}[^a-z])/gi, "</pre>");
+  // task list
+  text = text.replace(/([\*\-][\s]?\[x\])(.*)/g, `<p class="tasklist"><input type="checkbox" checked disabled/>$2</p>`);
+  text = text.replace(/([\*\-][\s]?\[\s?\])(.*)/g, `<p class="tasklist"><input type="checkbox" disabled/>$2</p>`);
+  // li - must be the last one
+  text = text.replace(/\*(.*)/g, `<p class="li">$1</p>`);
+  // quote
+  text = text.replace(/^[\>]{1}(.*)/gm, `<blockquote>$1</blockquote>`);
+  //! end MD
+
+  // line break
+  text = text.replace(br_regex, `<div class="br"></div>`);
+  // clean
+  text = text.replace(/([\`]{3})$/, "").trim();
+
   return text;
 }
