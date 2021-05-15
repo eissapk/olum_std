061b386fcaa6779b006c100b77fccb51b657472e
Eissa Saber <eissapk44@gmail.com>
2021-05-15 16:10:06 +0200

    updated example - markdown parser + drag events

--- src/services/parse.js
+++ src/services/parse.js
@@ -1,22 +1,35 @@
 const lt_regex = /</g;
 const gt_regex = />/g;
 const br_regex = /\n|\r|↵|\u21b5/g;
+const hash_regex = /\#/gi;
+const tag_regex = /<[^>]*>/gi;
+const space_regex = /\s/g;
 
-export function parse(text) {
+//* order matters
+export function parse(md) {
+  let text = md;
   // tags
-  text = text.replace(/<[^>]*>/gi, str => str.replace(lt_regex, "&lt;").replace(gt_regex, "&gt;"));
+  text = text.replace(tag_regex, str => str.replace(lt_regex, "&lt;").replace(gt_regex, "&gt;"));
 
   //! start MD
   // line
-  text = text.replace(/---|___/g, `<hr>`);
-  // font styles
-  text = text.replace(/([\*\_]{2})(.*)([\*\_]{2})/g, "<b>$2</b>");
-  text = text.replace(/([\~]{2})(.*)([\~]{2})/gi, "<del>$2</del>");
-  text = text.replace(/([\*\_]{1})(.*)([\*\_]{1})/gi, "<i>$2</i>");
+  text = text.replace(/[\-\_]{3}/g, `<hr>`);
   // image - must before link
   text = text.replace(/\!\[([^\]]+)\]\(([^\)]+)\)/gi, `<img src="$2" alt="$1" />`);
   // link
   text = text.replace(/\[([^\]]+)\]\(([^\)]+)\)/gi, `<a href="$2" target="_blank">$1</a>`);
+  //? fix urls src & href attributes
+  text = text.replace(/src=\"([^"]*)\"|href=\"([^"]*)\"/gi, str => {
+    return str.replace(/\_/g, "&lowbar;").replace(/\#/g, "&num;").replace(/\*/g, "&ast;").replace(/\~/g, "&tilde;");
+  });
+  // font styles
+  text = text.replace(/[\_\*]{2}([^\_\*].*?)[\_\*]{2}/g, "<b>$1</b>");
+  text = text.replace(/[\~]{2}([^\~].*?)[\~]{2}/gi, "<del>$1</del>");
+  text = text.replace(/[\_\*]{1}([^\_\*].*?)[\_\*]{1}/g, "<i>$1</i>");
+  // code
+  text = text.replace(/[\`]{3}([a-z]+)([^\`]+)[\`]{3}/gi, `<pre><code class="language-$1">$2</code></pre>`);
+
+  text = text.replace(/<pre[\s\S]*?>[\s\S]*?<\/pre>/gi, str => str.replace(hash_regex, "&num;"));
   // heading - must be a descending order
   text = text.replace(/[#]{6}(.*)/gi, "<h6>$1</h6>");
   text = text.replace(/[#]{5}(.*)/gi, "<h5>$1</h5>");
@@ -24,22 +37,22 @@ export function parse(text) {
   text = text.replace(/[#]{3}(.*)/gi, "<h3>$1</h3>");
   text = text.replace(/[#]{2}(.*)/gi, "<h2>$1</h2>");
   text = text.replace(/[#]{1}(.*)/gi, "<h1>$1</h1>");
-  // code
-  text = text.replace(/([\`]{3}[a-z]+)/gi, "<pre>");
-  text = text.replace(/([\`]{3}[^a-z])/gi, "</pre>");
   // task list
   text = text.replace(/([\*\-][\s]?\[x\])(.*)/g, `<p class="tasklist"><input type="checkbox" checked disabled/>$2</p>`);
   text = text.replace(/([\*\-][\s]?\[\s?\])(.*)/g, `<p class="tasklist"><input type="checkbox" disabled/>$2</p>`);
   // li - must be the last one
   text = text.replace(/\*(.*)/g, `<p class="li">$1</p>`);
-  // quote
-  text = text.replace(/^[\>]{1}(.*)/gm, `<blockquote>$1</blockquote>`);
+  // quote code
+  text = text.replace(/^[\>]{1}(.*)/gm, `<blockquote><p>$1</p></blockquote>`);
+  // quote text
+  text = text.replace(/\`([^\`].*?)\`/gi, `<p class="quoteText">$1</p>`);
   //! end MD
 
   // line break
   text = text.replace(br_regex, `<div class="br"></div>`);
-  // clean
-  text = text.replace(/([\`]{3})$/, "").trim();
+  //? fix non-breaking space
+  text = text.replace(space_regex, "&nbsp;");
+  text = text.replace(tag_regex, str => str.replace(/&nbsp;/g, " "));
 
-  return text;
+  return text.trim();
 }
