455a9096694170b3e35db5b39207dc1abeb81546
Eissa Saber <eissapk44@gmail.com>
2021-07-02 02:55:53 +0200

    optimized & fixed compiler & added autopefixer

--- compiler.js
+++ compiler.js
@@ -2,20 +2,32 @@
 const commander = require("commander");
 const cmd = new commander.Command();
 const sass = require("sass");
-const autoprefixer = require('autoprefixer');
-const postcss = require('postcss');
+const autoprefixer = require("autoprefixer");
+const postcss = require("postcss");
 const fs = require("fs");
 const extra = require("fs-extra");
 const path = require("path");
 const colors = require("colors");
 const settings = require("./settings");
+
+// helpers
 const isDebugging = true;
-const debugLib = arg => isDebugging ? console.log(arg) : "";
+const debugLib = arg => (isDebugging ? console.log(arg) : "");
+const quotes = (msg, color = "grey") => `'${colors[color].bold(msg)}'`;
+const log = (type, path, err) => quotes(`${type} : ${path.replace(this.sub, "src")}`, "white") + "\n" + colors.red.bold(err);
+const isObj = obj => !!(obj !== null && typeof obj === "object");
+const isFullArr = arr => !!(isObj(arr) && Array.isArray(arr) && arr.length);
+const isDef = val => !!(val !== undefined && val !== null);
+const flatten = lists => lists.reduce((a, b) => a.concat(b), []);
+const mkRegex = arr => {
+  const pattern = arr.map(str => `${str.toLowerCase().trim()}|`).join("").slice(0, -1);
+  const regex = new RegExp(pattern, "gi");
+  return regex;
+};
 
 class Compiler {
+  viewsDirs = ["components", "views"];
   sub = settings.src;
-  mainDirs = ["components", "views"];
-  dirs = [];
   regex = {
     template: {
       all: /<template[\s\S]*?>[\s\S]*?<\/template>/gi,
@@ -33,133 +45,69 @@ class Compiler {
     },
   };
 
-  dirsRegex(arr) {
-    const pattern = arr
-      .map(dir => `${dir.toLowerCase().trim()}|`)
-      .join("")
-      .slice(0, -1);
-    const regex = new RegExp(pattern, "gi");
-    return regex;
-  }
-
-  compsDirs() {
-    const dirs = src => {
-      return fs
-        .readdirSync(src)
-        .map(item => path.join(src, item))
-        .filter(item => fs.statSync(item).isDirectory());
-    };
-    const recursive = src => [src, ...this.flatten(dirs(src).map(recursive))];
-    let arr = recursive("src");
-
-    arr = arr
-      .filter(item => {
-        const regex = this.dirsRegex(this.mainDirs);
-        const result = regex.test(item);
-        if (result) return item;
-      })
-      .map(item => item.replace(/^src/, this.sub));
-
-    this.dirs = arr; // update
-  }
-
-  flatten(lists) {
-    return lists.reduce((a, b) => a.concat(b), []);
-  }
-
   constructor() {
-    this.compsDirs(); // get all directories paths in components & views
-
     cmd.command("clean").action(this.clean.bind(this));
     cmd.command("compile").action(this.init.bind(this));
     cmd.parse(process.argv);
   }
 
-  html(file) {
-    const tempArr = file.match(this.regex.template.all);
-    const template = Array.isArray(tempArr) && tempArr.length ? tempArr[0] : ""; // get 1st template tag
-    const markup = template.replace(this.regex.template.tag, "");
-    return "\n template() { \n return `" + markup + "`;\n}\n";
-  }
+  getPaths(base = "src") {
+    return new Promise((resolve, reject) => {
+      const dirs = src => fs.readdirSync(src).map(item => path.join(src, item)).filter(item => fs.statSync(item).isDirectory());
+      const recursive = src => [src, ...flatten(dirs(src).map(recursive))];
+      const arr = recursive(base);
+      
+      // get all dirs 
+      const directories = [...arr].filter(item => {
+          const regex = mkRegex(this.viewsDirs);
+          const result = regex.test(item);
+          if (result) return item;
+      });
+      if (!isFullArr(directories)) return reject("No directories found !");
+      const lastDir = directories[directories.length-1];
 
-  hasSASS(style) {
-    const regex = new RegExp("lang=['\"]scss|sass['\"]", "gi");
-    const openTagArr = style.match(this.regex.style.openTag);
-    const openTag = Array.isArray(openTagArr) && openTagArr.length ? openTagArr[0] : "";
-    return regex.test(openTag);
-  }
+      // get all files 
+      this.paths = [];
+      directories.forEach((dir, index) => {
+        fs.readdir(path.resolve(dir), (err, files) => {
+          if (err) return reject(err);
 
-  css(file, shared) {
-    const styleArr = file.match(this.regex.style.all);
-    let style = Array.isArray(styleArr) && styleArr.length ? styleArr[styleArr.length - 1] : ""; // get last style tag as the order of the component file cause you may have style tag inside the class "script tag"
-    const scss = style.replace(this.regex.style.tag, "");
-    const compiledShared = sass.renderSync({ data: shared }).css.toString();
-    const css = this.hasSASS(style) ? sass.renderSync({ data: shared + scss }).css.toString() : compiledShared + scss;
-    // prefix css
-    postcss([ autoprefixer ]).process(css, { from: undefined }).then(result => {
-      result.warnings().forEach(warn => console.warn(warn.toString()));
-      const finalStyle = "\n style() { \n return `" + result.css + "`;\n}\n";
-      console.log(colors.green(finalStyle));
-    });
-    return "\n style() { \n return `" + css + "`;\n}\n";
-  }
+          files.forEach((file, ind) => {
+            const lastFile = files[files.length-1];
+            const item = path.join(dir, file);
+            if (!fs.statSync(item).isDirectory()) this.paths.push(item.trim().replace(/^src/, this.sub)); // push files only and exclude directories
+            if (directories.indexOf(lastDir) === index && files.indexOf(lastFile) === ind) {
+              const msg = `Created ${quotes("Paths", "blue")} Tree.`;
+              debugLib(msg);
+              resolve();
+            }
+          });
+          
+        });
+      });
 
-  js(file) {
-    const scriptArr = file.match(this.regex.script.all);
-    let script = Array.isArray(scriptArr) && scriptArr.length ? scriptArr[0] : "";
-    return script.replace(this.regex.script.tag, "");
-  }
-
-  fileName(file) {
-    const arr = file.split(".");
-    const extension = arr[arr.length - 1];
-    return file.replace(extension, "") + "js";
-  }
-
-  createFile(dir, fileName, compiledFile) {
-    fs.writeFile(path.resolve(__dirname, dir, fileName), compiledFile, err => {
-      if (err) return console.error(colors.red.bold(err));
-      debugLib(colors.blue.bold(`Created: "${fileName}"`));
-    });
-  }
-
-  removeFile(dir, file) {
-    extra.remove(path.resolve(__dirname, dir, file), err => {
-      if (err) return console.error(colors.red.bold(err));
-      debugLib(colors.yellow.bold(`Removed "${file}"`));
-    });
-  }
-
-  compile(dir, file, shared) {
-    fs.readFile(path.resolve(__dirname, dir, file), "utf8", (err, data) => {
-      if (err) return console.error(colors.red.bold(err));
-
-      const html = this.html(data);
-      const css = this.css(data, shared);
-      const js = this.js(data);
-      const compiledFile = js.replace(this.regex.script.curlyAfterClass, "{" + html + css);
-
-      const fileName = this.fileName(file);
-      this.createFile(dir, fileName, compiledFile);
-      this.removeFile(dir, file);
     });
   }
 
   clean() {
     return new Promise((resolve, reject) => {
-      extra.remove(path.resolve(__dirname, this.sub), err => {
-        if (err) reject(err);
-        debugLib(colors.yellow.bold(`Removed "${this.sub}" directory`));
-        resolve();
-      });
+      if (fs.existsSync(this.sub)) { // if folder exists
+        extra.remove(path.resolve(__dirname, this.sub), err => {
+          if (err) return reject(err);
+          const msg = `Deleted ${quotes(this.sub, "red")} Directory.`;
+          debugLib(msg);
+          resolve();
+        });
+      } else resolve();
     });
   }
 
   copy() {
     return new Promise((resolve, reject) => {
       extra.copy(path.resolve(__dirname, "src"), path.resolve(__dirname, this.sub), err => {
-        if (err) reject(err);
-        debugLib(colors.green.bold('Cloned "src" directory'));
+        if (err) return reject(err);
+        const msg = `Copied & renamed ${quotes("src", "green")} Directory → ${quotes(this.sub, "yellow")}`;
+        debugLib(msg);
         resolve();
       });
     });
@@ -170,8 +118,10 @@ class Compiler {
     return new Promise((resolve, reject) => {
       if (fs.existsSync(file)) {
         fs.readFile(file, "utf8", (err, data) => {
-          if (err) reject(err);
-          resolve(data);
+          if (err) return reject(err);
+          const msg = `Copied ${quotes("shared", "green")} Style.`;
+          debugLib(msg);
+          resolve(data.trim());
         });
       } else {
         resolve("");
@@ -179,26 +129,136 @@ class Compiler {
     });
   }
 
-  update(shared = "") {
-    this.dirs.forEach(dir => {
-      fs.readdir(path.resolve(__dirname, dir), (err, files) => {
-        if (err) return console.error(colors.red.bold(err));
+  compiledSharedStyle(shared) {
+    const file = path.resolve(__dirname, "src/styles/shared.scss");
+    return new Promise((resolve, reject) => {
+      try {
+        // compile sass to css 
+        const compiled = sass.renderSync({ data: shared }).css.toString();
+        resolve(compiled);
+      } catch (err) { // error related to Sass
+        reject(log("Sass Compiler", file, err));
+      }
+    });
+  }
 
-        files.forEach(file => {
-          const item = path.join(dir, file);
-          // get files only and exclude directories
-          if (!fs.statSync(item).isDirectory()) this.compile(dir, file, shared);
+  hasSASS(style) {
+    const regex = new RegExp("lang=['\"]scss|sass['\"]", "gi");
+    const openTagArr = style.match(this.regex.style.openTag);
+    const openTag = isFullArr(openTagArr) ? openTagArr[0] : "";
+    return regex.test(openTag);
+  }
+
+  css(file, data, shared, compiledShared) {
+    return new Promise((resolve, reject) => {
+      const styleArr = data.match(this.regex.style.all);
+      const style = isFullArr(styleArr) ? styleArr[styleArr.length - 1] : ""; // get last style tag as the order of component file | because you may have style tag inside the class "script tag"
+      const scss = style.replace(this.regex.style.tag, "");
+      
+      try {
+        // compile sass to css 
+        const css = this.hasSASS(style) ? sass.renderSync({ data: shared + scss }).css.toString() : compiledShared + scss;
+        // prefix compiled css
+        postcss([autoprefixer]).process(css, { from: undefined }).then(result => {
+          result.warnings().forEach(warn => console.warn(colors.yellow.bold(warn.toString())));
+          const finalStyle = "\n style() { \n return `" + result.css + "`;\n}\n";
+          resolve(finalStyle);
+        }).catch(err => reject(log("PostCSS Compiler", file, err.reason + "\n" + err.showSourceCode())));
+      
+      } catch (err) { // error related to Sass
+        reject(log("Sass Compiler", file, err));
+      }
+    });
+  }
+
+  html(data) {
+    return new Promise(resolve => {
+      const tempArr = data.match(this.regex.template.all);
+      const template = isFullArr(tempArr) ? tempArr[0] : ""; // get 1st template tag as the order of component file
+      const markup = template.replace(this.regex.template.tag, "");
+      const html = "\n template() { \n return `" + markup + "`;\n}\n";
+      resolve(html);
+    });
+  }
+
+  js(file, data) {
+    return new Promise((resolve, reject) => {
+      const scriptArr = data.match(this.regex.script.all);
+      const script = isFullArr(scriptArr) ? scriptArr[0] : ""; // get 1st script tag as the order of component file
+      const js = script.replace(this.regex.script.tag, "");
+      const hasJsClass = this.regex.script.curlyAfterClass.test(js);
+      if (hasJsClass) resolve(js);
+      else reject(log("JS Class", file, "Couldn't find a class or opening curly bracket e.g. class Example '{' is followed with code"));
+    });
+  }
+
+  modifyExt(file) {
+    const arr = file.split(".");
+    const ext = arr[arr.length - 1];
+    return file.replace(ext, "") + "js";
+  }
+
+  merge(html, css, js) {
+    return new Promise(resolve => {
+      const file = js.replace(this.regex.script.curlyAfterClass, "{" + html + css);
+      resolve(file);
+    });
+  }
+  
+  createFile(oldPath, newPath, compiledFile) {
+    return new Promise((resolve, reject) => {
+      fs.rename(oldPath, newPath, err => {
+        if (err) return reject(err);
+        fs.writeFile(newPath, compiledFile, err => {
+          if (err) return reject(err);
+          // debugLib("Created " + quotes(path.basename(newPath), "green"));
+          resolve();
         });
       });
     });
   }
 
+  update(shared, compiledShared) {
+    if (isDef(this.paths) && isFullArr(this.paths)) {
+      const recursive = num => {
+        const file = this.paths[num].trim();
+        const newPath = this.modifyExt(file);
+
+        fs.readFile(file, "utf8", (err, data) => {
+          if (err) return console.error(colors.red.bold(err));
+
+          (async () => {
+            try {
+              const html = await this.html(data);
+              const css = await this.css(file, data, shared, compiledShared);
+              const js = await this.js(file, data);
+              const compiledFile = await this.merge(html, css, js);
+              await this.createFile(file, newPath, compiledFile);
+
+              if (num + 1 <= this.paths.length - 1) recursive(num + 1); // next
+            } catch (err) {
+              console.error(err);
+            }
+          })();
+
+        });
+      };
+      recursive(0);
+    } else {
+      const msg = `No ${quotes("components", "red")} to compile.`;
+      console.warn(msg);
+    }
+  }
+
   async init() {
     try {
+      await this.getPaths("src");
       await this.clean();
       await this.copy();
       const shared = await this.sharedStyle();
-      this.update(shared);
+      const compiledShared = await this.compiledSharedStyle(shared);
+      this.update(shared, compiledShared);
+      debugLib(`Updated ${quotes("Components", "green")}`);
     } catch (err) {
       console.error(colors.red.bold(err));
     }
