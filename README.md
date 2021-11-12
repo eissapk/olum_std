ad25aa30b10d018ede0e97716d3c72e93cee886d
Eissa Saber <eissapk44@gmail.com>
2021-11-13 01:42:36 +0200

    init

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# olum-std
+# todo-list
 
 ## Installation
 ```
--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import Olum from "olum";
-import { Localize } from "olum-helpers";
-import router from "./router/index.js";
-import Home from "./views/home.js";
-import en from "./locales/en.js";
-import ar from "./locales/ar.js";
+import router from "./router";
 
-// translations
-new Localize({ en, ar }, ["ar"]);
-console.log("olumDesc".trans());
-// new Olum({ prefix: "app" }).$("#app").use(Home);
-new Olum({ prefix: "app" }).$("#app").use(router);
-
+new Olum().$("#root").use(router);
 
 // if ("serviceWorker" in navigator) { // uncomment to enable service worker when deploying
 //   window.on("load", () => navigator.serviceWorker.register("/service-worker.js").catch(console.error));
