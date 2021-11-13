680084880d86bc44907d9c100601f181ba804265
Eissa Saber <eissapk44@gmail.com>
2021-11-13 02:05:29 +0200

    init

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# todo-list
+# starter
 
 ## Installation
 ```
--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,8 @@ import OlumRouter from "olum-router";
 import Home from "../views/Home";
 import About from "../views/About";
 
-const routes = [
-  { path: "/", comp: Home },
-  { path: "/about", comp: About },
-];
+const routes = [ { path: "/", comp: Home }, { path: "/about", comp: About } ];
 
-const router = new OlumRouter({ routes, mode: "history" });
+const router = new OlumRouter({ routes });
+
 export default router;
