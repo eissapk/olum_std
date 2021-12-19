cbe0d032335a2d417f7b884bce803bf50523916a
Eissa Saber <eissapk44@gmail.com>
2021-12-20 00:37:28 +0200

    initial test

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import OlumRouter from "olum-router";
 import Home from "../views/Home";
-import About from "../views/About";
 
-const routes = [ { path: "/", comp: Home }, { path: "/about", comp: About } ];
+const routes = [ { path: "/", comp: Home } ];
 
 const router = new OlumRouter({ routes });
 
